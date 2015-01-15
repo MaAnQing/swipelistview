1ff151a1b0469a2440bf538ccbe3e894b90bfce4
maanqing <madroidmaq@gmail.com>
2015-01-15 10:14:24 +0800

    fix details

--- app/src/main/java/com/huami/madroid/swipelistview/MainActivity.java
+++ app/src/main/java/com/huami/madroid/swipelistview/MainActivity.java
@@ -3,8 +3,6 @@ package com.huami.madroid.swipelistview;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -27,7 +25,7 @@ public class MainActivity extends ActionBarActivity {
     private void init(){
         listView = (ListView)findViewById(R.id.listview_main) ;
 
-        String[] dataSourceList = {"listview Demo1" ,"listview Demo2"} ;
+        String[] dataSourceList = {"SlideCutListView Demo1" ,"SwipeDelListView Demo2"} ;
 
         adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,dataSourceList) ;
 
@@ -40,12 +38,12 @@ public class MainActivity extends ActionBarActivity {
                 switch (position){
                     case 0:
                         openActivity(SlideCutActivity.class);
-                        showToast("listview item is clicked") ;
-                       // Toast.makeText(MainActivity.this,"listview item is clicked",Toast.LENGTH_LONG).show();
+                        showToast("listView item is clicked") ;
+
                         break;
                     case 1:
                         openActivity(SwipeDelListViewActivity.class);
-                        showToast("swipeDelListview is clicked");
+                        showToast("swipeDelListView is clicked");
                         break;
                     default:
                         break;
@@ -55,33 +53,13 @@ public class MainActivity extends ActionBarActivity {
     }
 
     private void openActivity(Class name){
-        Intent slidecutIntent = new Intent() ;
-        slidecutIntent.setClass(this,name) ;
-        startActivity(slidecutIntent);
+        Intent slideCutIntent = new Intent() ;
+        slideCutIntent.setClass(this,name) ;
+        startActivity(slideCutIntent);
     }
 
     private void showToast(String msg){
         Toast.makeText(MainActivity.this,msg,Toast.LENGTH_LONG).show();
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
